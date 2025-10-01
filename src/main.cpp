0daae3c6cc5670e915d0827bacef933bbe2860c9
GabyDs <gabydsch2001@gmail.com>
2025-10-01 15:20:39 -0300

    Connect broker mqtt and add more boards

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,43 @@
-#include <Arduino.h>
+#include <WiFi.h>
+#include <PicoMQTT.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char* ssid = "Wi-FiUnamBots";
+const char* password = "fiounambots";
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// Configure static IP to be on the same network as the broker
+IPAddress local_IP(10, 0, 0, 200);    // IP different from broker (99)
+IPAddress gateway(10, 0, 0, 100);      // Gateway
+IPAddress subnet(255, 0, 0, 0);
+
+void conectarWiFi() {
+    WiFi.config(local_IP, gateway, subnet);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+PicoMQTT::Client mqtt(
+  "10.0.0.100",   // Broker
+  1883,             // Port
+  "Robot1",   // Client ID
+  "user1",          // Username
+  "1234"            // Password
+);
+
+void setup(){
+    // Conectar al WiFi
+    conectarWiFi();
+
+    mqtt.begin();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop(){
+    // Verificar conexión WiFi cada ciclo
+    if (WiFi.status() != WL_CONNECTED) {
+        conectarWiFi();
+    }
+
+    mqtt.loop();
+    mqtt.publish("picomqtt/welcome", "Hello from PicoMQTT!");
+    
+    delay(5000); // Verificar cada 5 segundos
 }
